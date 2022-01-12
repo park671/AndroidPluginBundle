c7cfccf5f868d493c478ed1296002d20d464789a
yucong <yucong@kuaishou.com>
2022-01-12 14:39:50 +0800

    first commit

---
+++ app/src/main/java/com/park/plugin/LayoutActivity.java
@@ -0,0 +1,25 @@
+package com.park.plugin;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class LayoutActivity extends BaseActivity {
+
+  Button mButton;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    mButton = findViewById(R.id.testButton);
+    mButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Toast.makeText(LayoutActivity.this, "随便弹点什么内容", Toast.LENGTH_SHORT).show();
+      }
+    });
+  }
+}
---
+++ app/src/main/java/com/park/plugin/MainActivity.java
@@ -0,0 +1,35 @@
+package com.park.plugin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends BaseActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    LinearLayout linearLayout = new LinearLayout(this);
+    linearLayout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+    linearLayout.setGravity(Gravity.CENTER);
+    linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+    TextView textView = new TextView(MainActivity.this);
+    textView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+    textView.setGravity(Gravity.CENTER);
+    linearLayout.addView(textView);
+    textView.setText("I am plugin activity.");
+
+    ImageView imageView = new ImageView(MainActivity.this);
+    imageView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+    linearLayout.addView(imageView);
+    imageView.setImageDrawable(getResources().getDrawable(R.drawable.test_img));
+    imageView.setOnClickListener(v-> startActivity(new Intent(MainActivity.this, LayoutActivity.class)));
+
+    setContentView(linearLayout);
+  }
+}
