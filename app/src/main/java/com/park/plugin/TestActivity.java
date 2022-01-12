716d0e6ca62e35a0ec7b815175ea7e7e454df96e
yucong <yucong@kuaishou.com>
2022-01-12 20:34:50 +0800

    update:添加webview页面

--- app/src/main/java/com/park/plugin/TestActivity.java
+++ app/src/main/java/com/park/plugin/TestActivity.java
@@ -2,6 +2,7 @@ package com.park.plugin;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class TestActivity extends BaseActivity {
   @Override
@@ -9,6 +10,13 @@ public class TestActivity extends BaseActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_test);
     WebView webView = findViewById(R.id.wbview);
+    webView.setWebViewClient(new WebViewClient() {
+      @Override
+      public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        view.loadUrl(url);
+        return super.shouldOverrideUrlLoading(view, url);
+      }
+    });
     webView.loadUrl("https://baidu.com");
   }
 }
